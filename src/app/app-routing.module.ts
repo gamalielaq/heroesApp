80f050a69e7a9aea8b6ae1f3ffd3c7c35e9b4a8b
Gamaliel <gabanto@sigcomt.com>
2021-02-03 17:25:26 -0500

    v1 uso del HTTP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'horoes', component: HeroesComponent},
+  {path: 'heroe/:id', component: HeroeComponent},
+  {path: '**',pathMatch: 'full' , redirectTo: 'horoes'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
